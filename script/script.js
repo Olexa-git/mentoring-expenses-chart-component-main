8dff7b6dc96228119dedacf4e62c20a9baf0f071
Olexa-git <oplutaev@gmail.com>
2023-01-13 23:05:16 +0200

    creating colums using data from json file

--- script/script.js
+++ script/script.js
@@ -13,9 +13,22 @@ xhr.onload = function() {
       count+=1;
   };
   const largest_colum = Math.max(...amoumt_list);
-  console.log(amoumt_list);
-  console.log(amoumt_list[0]+amoumt_list[1]);
-  console.log(Math.max(...amoumt_list));
+  let list_colum_height = [];
+  for (const item of res) {
+    list_colum_height.push(Math.round(160/largest_colum*item.amount));
+  }
+  let colum_list = document.querySelectorAll(".spending_colum");
+  count = 0
+  for (const item of colum_list) {
+    if (list_colum_height[count] == 160) {
+      item.style.height = "160px";
+      item.style.backgroundColor = "hsl(186, 34%, 60%)";
+    } else {
+      item.style.height = list_colum_height[count].toString()+'px';
+      item.style.backgroundColor = "hsl(10, 79%, 65%)";
+    }
+    count+=1
+  }
 };
 xhr.send();
 
@@ -24,7 +37,7 @@ function spending_item_content (day, amount, count) {
   let sp_item = `<li class="spending_item">
           <div class="spending_busket">
             <p class="spending_usd_sum">$${amount}</p>
-            <div class="spending_colum_${count}"></div>
+            <div class="spending_colum" id="spending_colum"></div>
           </div>
           <p class="spending_week_day">${day}</p>
         </li>`
